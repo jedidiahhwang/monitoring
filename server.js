2c772c54feda5242a2aad0ad29d4e5003f541f6d
Jeddy Hwang <jedidiah.hwang@gmail.com>
2021-09-13 15:05:41 -0600

    added procfile and env port

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
-app.listen(4545, () => {
-    console.log("They're taking the Hobbits to 4545!")
+const port = process.env.PORT || 4545;
+
+// app.listen(4545, () => {
+//     console.log("They're taking the Hobbits to 4545!")
+// })
+
+app.listen(port, () => {
+    console.log(`They're taking the Hobbits to ${port}`)
 })
 
 // 1) After typing all the above, and in the HTML, visit localhost:4545
+
+// git pull origin main --allow-unrelated-histories
