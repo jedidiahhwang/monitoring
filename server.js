d41fa77b705ecca9dfe9736f8902705c80bb9318
Jeddy Hwang <jedidiah.hwang@gmail.com>
2021-09-13 16:13:13 -0600

    added final post endpoitn

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ let rollbar = new Rollbar({
 const students = []; // Add this next line last
 const app = express();
 app.use(express.json()); // This is why rollbar gives you error, put this last
+app.use("/style", express.static("./public/styles.css")); // If anyone tries to load style, go to the static path instead
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -18,13 +19,34 @@ app.get("/", (req, res) => {
 })
 
 // Create post request
-app.post("/api/student", (req, res) => {
-    let {name} = req.body; // This needs to be let (used to be const)
-    name = name.trim();
-    students.push(name);
+// app.post("/api/student", (req, res) => {
+//     let {name} = req.body; // This needs to be let (used to be const)
+//     name = name.trim();
+//     students.push(name);
 
-    rollbar.log("Student added successfully", {author: "Jeddy", type: "Manual entry"})
-    res.status(200).send(students);
+//     rollbar.log("Student added successfully", {author: "Jeddy", type: "Manual entry"})
+//     res.status(200).send(students);
+// })
+
+// Additional post request
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 const port = process.env.PORT || 4545;
