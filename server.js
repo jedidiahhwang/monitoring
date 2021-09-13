e1048aa0b9badc6f2dc528c08684db143d76b071
Jeddy Hwang <jedidiah.hwang@gmail.com>
2021-09-13 15:27:46 -0600

    implemented rollbar

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
+const Rollbar = require("rollbar"); // Capitalize because it's a class
+
+let rollbar = new Rollbar({
+    accessToken: "847d4e03790b41c88baadf5d1a6529f5",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+}) // Takes in an object
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+    rollbar.info("html file served successfully!");
 })
 
 const port = process.env.PORT || 4545;
@@ -20,3 +28,6 @@ app.listen(port, () => {
 // 1) After typing all the above, and in the HTML, visit localhost:4545
 
 // git pull origin main --allow-unrelated-histories
+
+// 2) After a successful build in Heroku, npm i rollbar and import it
+// 3) Go to projects, "first project", tokens, create access token
